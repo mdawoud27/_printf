ff9236eb114e9f996cd493ec352711d53e758da8
dawoud <113205913+mdawoud27@users.noreply.github.com>
2023-08-25 18:12:32 +0300

    Update special_str.c

--- special_str.c
+++ special_str.c
@@ -170,12 +170,7 @@ int convert_int_to_str(int num, char *str)
 	}
 
 	// Reverse the string
-	for (int i = 0; i < length / 2; i++)
-	{
-		char temp = str[i];
-		str[i] = str[length - 1 - i];
-		str[length - 1 - i] = temp;
-	}
+	reverse_str(str, length);
 
 	// Null-terminate the string
 	str[length] = '\0';
